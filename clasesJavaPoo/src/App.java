15372b2585206b80b3b3a76ea934f05513f048b1
Nicolas-Cardona2 <nicolas.cardona.garcia@correounivalle.edu.co>
2025-09-16 11:42:59 -0500

    se crearon getters y setters

--- clasesJavaPoo/src/App.java
+++ clasesJavaPoo/src/App.java
@@ -4,13 +4,13 @@ public class App {
         String universidad = "Del valle";
         System.out.println(universidad);
         Humano humano1 = new Humano();
-        humano1.nombre = "Juan Alberto";
-        humano1.edad = 19;
-        Humano humano2 = new Humano("Nicolás", (byte)19);
-        System.out.println(humano2.nombre); //tenes una variable que basicamente esta creada en la clase humano, tenes que llamarla para que te imprima lo que necesitas
-        System.out.println(humano2.edad);
-        System.out.println(humano1.edad);
-        System.out.println(humano1.nombre);
+        humano1.setNombre("Juan Alberto");
+        humano1.setEdad((byte)30);
+        Humano humano2 = new Humano("Nicolás", (byte)19, "1117350032");
+        System.out.println(humano2.getNombre()); //tenes una variable que basicamente esta creada en la clase humano, tenes que llamarla para que te imprima lo que necesitas
+        System.out.println(humano2.getEdad());
+        System.out.println(humano1.getEdad());
+        System.out.println(humano1.getNombre());
         humano2.saludar();
         humano2.presentarse();
         humano1.presentarse();
--- clasesJavaPoo/src/Humano.java
+++ clasesJavaPoo/src/Humano.java
@@ -1,11 +1,13 @@
 public class Humano {
 
-    String nombre;//asi dejas los datos vacios para agregarlos en el otro archivo
-    byte edad;
+    private String nombre;//asi dejas los datos vacios para agregarlos en el otro archivo
+    private byte edad;
+    private String identificacion;
 
-    public Humano(String nuevoNombre, byte nuevaEdad){// creas un constructor como este para datos que quieres tener ya listos o implementados en el otro archivo
+    public Humano(String nuevoNombre, byte nuevaEdad, String nuevaIdentificacion){// creas un constructor como este para datos que quieres tener ya listos o implementados en el otro archivo
         nombre = nuevoNombre;
         edad = nuevaEdad;
+        identificacion = nuevaIdentificacion;
     }
 
     public Humano(){
@@ -13,11 +15,11 @@ public class Humano {
     }
 
     public void saludar(){
-        System.out.println("Que pasa corazón");
+        System.out.println("Que pasa corazón, mi cedula es " + identificacion);
     }
 
     public void presentarse(){
-        System.out.println("Hola miamor, me llamo " + nombre);
+        System.out.println("Hola miamor, me llamo " + nombre + " y tengo " + edad);
     }
 
     public String estrarto(int salario){
@@ -28,4 +30,28 @@ public class Humano {
         }
 
     }
+
+    public String getNombre(){
+    return nombre;
+    }
+
+    public void setNombre(String nuevoNombre){
+        nombre = nuevoNombre;
+    }
+
+    public void setEdad(byte nuevaEdad){
+        edad = nuevaEdad;
+    }
+
+    public byte getEdad(){
+    return edad;
+    }
+
+    public String getIdentificacion(){
+    return identificacion;
+    }
+
+    /* se pueden crear los getter, setters y el constructor de una clase desde el mismo visual con click derecho -> source action y ahi 
+      elegit la opcion que desean 
+     */
 }
